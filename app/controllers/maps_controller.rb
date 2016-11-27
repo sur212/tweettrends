084e891d14d1dc11db78deaf73f39263fb42a4c0
suraj <sur212@nyu.edu>
2016-11-27 18:06:21 -0500

    ass 2

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,3 +1,4 @@
+require 'C:/Users/Shashank Pavan/Desktop/CloudComputing-Assignment-1/app/controllers/server_side.rb'
 require 'faraday_middleware/aws_signers_v4'
 require 'elasticsearch'
 
@@ -8,17 +9,23 @@ class MapsController < ApplicationController
   # GET /maps
   # GET /maps.json
   def index
-    @a=params[:search]
-    client          = Elasticsearch::Client.new(url: 'PUT-AWS-ELASTIC-SEARCH-URL') do |f|
+    @a              = params[:search]
+    client          = Elasticsearch::Client.new(url: 'http://search-twitterstream-ywbxov7tqypovgqglnjcuer4oq.us-west-2.es.amazonaws.com') do |f|
                       f.request :aws_signers_v4,
-                        credentials: Aws::Credentials.new( 'PASTE-YOUR-AWS-ACCESS-TOKEN', 'PASTE-YOUR-AWS-ACCESS-KEY' ),
+                        credentials: Aws::Credentials.new( 'AKIAICYMYADURIIPPTUQ', 'npB74E7xPzUt3atifX33DKQ0gHp24YmPzt138fmd' ),
                         service_name: 'es',
                         region: 'us-west-2'
                       end
-    elasticsearch   = client.search({from: 0, size: 10000, index: 'twittmaps', q: @a })["hits"]["hits"]
+    elasticsearch   = client.search({from: 0, size: 10000, index: 'surajtest', q: @a })["hits"]["hits"]
     result          = []
     elasticsearch.each do |es|
-      result.push({"latitude" => es["_source"]["latitude"], "longitude" => es["_source"]["longitude"]})
+      if es["_source"]["sentiment"] == "neutral"
+        result.push({"latitude" => es["_source"]["latitude"], "longitude" => es["_source"]["longitude"], "sentiment" => 0})
+      elsif es["_source"]["sentiment"] == "positive"
+        result.push({"latitude" => es["_source"]["latitude"], "longitude" => es["_source"]["longitude"], "sentiment" => 1})
+      elsif es["_source"]["sentiment"] == "negative"
+        result.push({"latitude" => es["_source"]["latitude"], "longitude" => es["_source"]["longitude"], "sentiment" => -1})
+      end
     end
     @maps = result
   end
@@ -40,17 +47,6 @@ class MapsController < ApplicationController
   # POST /maps
   # POST /maps.json
   def create
-    @map = Map.new(map_params)
-
-    respond_to do |format|
-      if @map.save
-        format.html { redirect_to @map, notice: 'Map was successfully created.' }
-        format.json { render :show, status: :created, location: @map }
-      else
-        format.html { render :new }
-        format.json { render json: @map.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /maps/1
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :maps
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'maps#index'
+  get 'messaging' => 'messaging#stream', :as => 'messaging'
 end
